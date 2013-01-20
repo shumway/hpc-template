5b64d3b9ed4f2ae2b2b9496d77018b8c2c38a7a3
John Shumway <john.shumwayjr@gmail.com>
2013-01-19 20:24:02 -0700

    Updated main.cpp to use "auto" C++11 feature.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char** argv) {
+    auto hello = new std::string("Hello World!");
+    std::cout << *hello << std::endl;
     return 0;
 }
