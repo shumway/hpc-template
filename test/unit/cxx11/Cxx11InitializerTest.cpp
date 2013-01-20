04570311dd37ed96ce6b93eee39844fec62575c6
John Shumway <john.shumwayjr@gmail.com>
2013-01-19 22:42:08 -0700

    Added list initializer unit test.

---
+++ test/unit/cxx11/Cxx11InitializerTest.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include <list>
+#include <string>
+#include <sstream>
+
+namespace {
+
+class Cxx11InitializerTest: public testing::Test
+{
+protected:
+    void SetUp() {
+    }
+
+    void TearDown() {
+    }
+
+    std::string to_string(std::list<int>& list) {
+        std::stringstream stream;
+        stream << "{ ";
+        for (auto item = list.begin(); item != list.end(); ++item) {
+            stream << (*item) << " ";
+        }
+        stream << "}";
+        return stream.str();
+    }
+
+};
+
+TEST_F(Cxx11InitializerTest, testList)  {
+    std::list<int> mylist = {1, 1, 2, 3, 5, 8, 13, 21};
+    ASSERT_EQ(size_t(8), mylist.size())
+        << "for list " << to_string(mylist);
+}
+
+
+}
